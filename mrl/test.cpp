38601d875481cc282cbd016b2e0b60d195eb5505
uberblah <miles.rufatlatre@colorado.edu>
2016-04-02 07:33:25 -0600

    a todo file for me

--- mrl/test.cpp
+++ mrl/test.cpp
@@ -10,7 +10,7 @@ int main()
     const unsigned int num_neurons_hidden = LAYERSIZE;
     const float desired_error = (const float) 0.0005;
     const unsigned int max_epochs = 500000;
-    const unsigned int epochs_between_reports = 1000;
+    const unsigned int epochs_between_reports = 100;
 
     struct fann *ann = fann_create_standard(num_layers, num_input,
 					    num_neurons_hidden, num_output);
